cd61947d37540dbe7cfb5fd583874622b6a2707c
jasaiful <jasaiful@gmail.com>
2023-08-20 22:47:33 +0600

    added total item and price calculation

--- scripts/item-cart.js
+++ scripts/item-cart.js
@@ -1,1 +1,32 @@
-console.log('cart list')
+
+let total = 0;
+
+function selectItemByClick(target){
+
+    const itemName = target.childNodes[3].childNodes[3].innerText;
+    const selectedItemsContainer = document.getElementById('selected-items');
+    const p = document.createElement('p');
+    const count = selectedItemsContainer.childElementCount;
+    p.classList.add('my-3');
+    p.classList.add('font-semibold');
+    p.classList.add('text-lg');
+    p.innerHTML = `${count + 1}. ${itemName}`;
+    selectedItemsContainer.appendChild(p);
+    
+
+    const priceText = target.childNodes[3].childNodes[5].innerText.split(" ")[0];
+    const price = parseFloat(priceText);
+    total = parseInt(total) + price;
+    const totalPurchase = total.toFixed(2);
+    document.getElementById('total-price').innerText = totalPurchase;
+
+    console.log(totalPurchase);
+
+    function buttonStyle(){
+        const applyButton = document.getElementById('');
+    }
+
+
+}
+
+
