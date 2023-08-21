dad81f55c8a4d97ade10c59096378e8e924cc3a2
jasaiful <jasaiful@gmail.com>
2023-08-21 21:18:24 +0600

    added coupon button condition

--- scripts/item-cart.js
+++ scripts/item-cart.js
@@ -25,39 +25,52 @@ function selectItemByClick(target) {
     total = parseInt(total) + price;
     const totalPurchase = total.toFixed(2);
     document.getElementById('total-price').innerText = totalPurchase;
+    document.getElementById('total').innerText = totalPurchase;
 
-
-
-    // purchase button
+    // purchase button condition
     if (total > 0) {
         purchaseButton.disabled = false;
     }
 
-    // apply button
+    // apply button condition
     if (total >= 200) {
         applyButton.disabled = false;
     }
 }
 
+const discountTextValue = document.getElementById('discount').innerText.split(" ")[0];
+const previousDiscountValue = parseFloat(discountTextValue);
+
+const totalTextValue = document.getElementById('total').innerText.split(" ")[0];
+totalTextValue.innerText = total;
+
+
+// coupon button condition
 
 document.getElementById('apply-btn').addEventListener('click', function () {
-
-    const discountTextValue = document.getElementById('discount').innerText.split(" ")[0];
-    const previousDiscountValue = parseFloat(discountTextValue);
-    // const discountValue = total * 20 %;
-
-
-    console.log(typeof discountValue, discountValue);
 
     const inputFieldValue = document.getElementById('coupon-field');
     const couponCode = inputFieldValue.value;
 
+    const discountCalculation = 0.2 * total;
+    const discountTotal = parseFloat(discountCalculation) + previousDiscountValue
+    const discount = discountTotal.toFixed(2);
+
+    const totalCalculation = total - discount
+    const totalAmount = totalCalculation.toFixed(2);
+
+    inputFieldValue.value = '';
+
     if (couponCode === "SELL200") {
-        discountTextValue = discountValue
+        document.getElementById('discount').innerText = discount;
+        document.getElementById('total').innerText = totalAmount;
     }
     else {
         alert('Please try again with a valid coupon code')
     }
 
+})
 
+document.getElementById('modal-btn').addEventListener('click', function () {
+    console.log("hello modal tell me how to clear shopping cart")
 })
